cad3603366d559cad764f172edc9b306facbb44f
Tiago Fernandes <tiago_c_fernandes@hotmail.com>
2024-01-22 11:15:25 +0000

    First blood

---
+++ Habits/Habits/HabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitsApp.swift
+//  Habits
+//
+//  Created by Tiago Fernandes on 22/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct HabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
