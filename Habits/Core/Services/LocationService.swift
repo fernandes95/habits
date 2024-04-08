2745cddb381bcfb2251ed2e46088f66a62f72c62
Tiago Fernandes <tiago_c_fernandes@hotmail.com>
2024-04-08 19:05:14 +0100

    Location Service improvements
    app now gets location updates on background

--- Habits/Core/Services/LocationService.swift
+++ Habits/Core/Services/LocationService.swift
@@ -19,7 +19,14 @@ class LocationService: NSObject, ObservableObject {
     override init() {
         super.init()
         self.locationManager.delegate = self
-        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        self.locationManager.distanceFilter = 10
+        self.locationManager.activityType = .otherNavigation
+        self.locationManager.allowsBackgroundLocationUpdates = true
+        self.locationManager.pausesLocationUpdatesAutomatically = false
+//        self.locationManager.stopUpdatingLocation()
+
+        self.locationManager.startUpdatingLocation()
     }
 
     func locationAuthorization() {
@@ -42,10 +49,46 @@ class LocationService: NSObject, ObservableObject {
 
 extension LocationService: CLLocationManagerDelegate {
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
-        self.status = manager.authorizationStatus
-        if manager.authorizationStatus == .authorizedWhenInUse {
+        let status: CLAuthorizationStatus = manager.authorizationStatus
+        self.status = status
+
+        switch status {
+        case .authorizedAlways:
+            // Handle case
+            return
+        case .authorizedWhenInUse:
             manager.requestAlwaysAuthorization()
+        case .denied:
+            // Handle case
+            return
+        case .notDetermined:
+            // Handle case
+            return
+        case .restricted:
+            // Handle case
+            return
+        default:
+            return
         }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            let latitude = location.coordinate.latitude
+            let longitude = location.coordinate.longitude
+            let message = "Changed location"
+
+            Task {
+                try await notificationService.requestInstantNotification(
+                    subTitle: message
+                )
+            }
+            print(message)
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        // Handle failure to get a user’s location
     }
 
     func locationManager(_ manager: CLLocationManager, didStartMonitoringFor region: CLRegion) {
@@ -54,22 +97,22 @@ extension LocationService: CLLocationManagerDelegate {
 
     func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
         if let region = region as? CLCircularRegion {
-//            Task {
-//                try await notificationService.requestInstantNotification(
-//                    subTitle: "Entered region with IDENTIFIER: \(region.identifier)"
-//                )
-//            }
+            Task {
+                try await notificationService.requestInstantNotification(
+                    subTitle: "Entered region with IDENTIFIER: \(region.identifier)"
+                )
+            }
             print("Entered region with IDENTIFIER: \(region.identifier)")
         }
     }
 
     func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
         if let region = region as? CLCircularRegion {
-//            Task {
-//                try await notificationService.requestInstantNotification(
-//                    subTitle: "Exited region with IDENTIFIER: \(region.identifier)"
-//                )
-//            }
+            Task {
+                try await notificationService.requestInstantNotification(
+                    subTitle: "Exited region with IDENTIFIER: \(region.identifier)"
+                )
+            }
             print("Exited region with IDENTIFIER: \(region.identifier)")
         }
     }
